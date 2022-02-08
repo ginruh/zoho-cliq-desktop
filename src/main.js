46896fd02038e5980b51afddaf9934eecd18e4e1
iyorozuya <iyaksha@tutanota.com>
2022-02-08 18:07:41 +0530

    fix: only one instance of app is allowed

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ const { app, BrowserWindow, Menu, nativeImage, session, Tray } = require('electr
 const path = require('path')
 
 let mainWindow = null
+let tray = null
+
+const appLock = app.requestSingleInstanceLock()
 
 function createWindow () {
   // Create the browser window
@@ -62,54 +65,53 @@ function createWindow () {
   )
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', () => {
-  createWindow()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-let tray = null
-app.whenReady().then(() => {
-  const icon = nativeImage.createFromPath(
-    path.join(__dirname, 'assets', 'icons', '256x256.png')
-  )
-  tray = new Tray(icon)
-
-  const contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'Open',
-      click: () => {
-        if (mainWindow) mainWindow.show()
-      }
-    },
-    {
-      label: 'Quit',
-      click: () => {
-        app.isQuiting = true
-        app.quit()
-      }
+if (!appLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory, additionalData) => {
+    if (mainWindow) {
+      if (!mainWindow.isVisible() || mainWindow.isMinimized()) mainWindow.show()
+      mainWindow.focus()
     }
-  ])
+  })
 
-  tray.setToolTip('Zoho Cliq')
-  tray.setContextMenu(contextMenu)
-})
+  app.on('ready', () => {
+    createWindow()
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+    app.on('activate', function () {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+  })
 
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
+  app.whenReady().then(() => {
+    const icon = nativeImage.createFromPath(
+      path.join(__dirname, 'assets', 'icons', '256x256.png')
+    )
+    tray = new Tray(icon)
+
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: 'Open',
+        click: () => {
+          if (mainWindow) mainWindow.show()
+        }
+      },
+      {
+        label: 'Quit',
+        click: () => {
+          app.isQuiting = true
+          app.quit()
+        }
+      }
+    ])
+
+    tray.setToolTip('Zoho Cliq')
+    tray.setContextMenu(contextMenu)
+  })
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+}
